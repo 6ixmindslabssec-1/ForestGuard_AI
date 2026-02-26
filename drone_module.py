ab4cbf08b4065429ae9508ec6ff0669414600e7a
6ixmindslabssec-1 <6ixmindslabssec@gmail.com>
2026-02-27 04:13:48 +0530

    Update drone_module.py

--- drone_module.py
+++ drone_module.py
@@ -1,1 +1,10 @@
+import random
 
+def detect_intruder(image_path):
+    print("🚁 Running drone object detection...")
+
+    probability = random.uniform(0, 1)
+
+    print(f"Suspicious Activity Probability: {probability:.2f}")
+
+    return probability > 0.6
